a6a29b98e107b3b20d70a1ed1600a8cbe4de9fb4
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-07-13 17:58:47 +0300

    Create main layout component

---
+++ src/layout/layout.js
@@ -0,0 +1,16 @@
+//layout component
+import React from 'react';
+import Users from '../users/usersComponent.js';
+
+class Layout extends React.Component {
+
+  render() {
+    return ( 
+		 <div>
+		 	<Users />
+		 </div>
+    );
+  }
+}
+
+export default Layout;
