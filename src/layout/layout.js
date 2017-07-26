98d5b0cad7eae4a30fa8641f3edc06d02aa61872
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-07-26 12:02:37 +0300

    Fix table position

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -10,7 +10,7 @@ class Layout extends React.Component {
   render() {
     return (
 		  <div>
-				<div className="container">
+				<div className="container height">
 
 					<div className="row">
 						<SumMetrics />
