c46be39787f5ab1f60720dfdad84a523a76c7d65
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-07-31 15:37:42 +0300

    Add sidebar

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,39 +1,19 @@
-//layout component
 import React from 'react';
 import SumMetrics from '../metrics/sumMetricsComponent.js';
-import Stats from '../statistics/statsComponent.js';
-import Projects from '../projects/projectsComponent.js';
-import Topics from '../topics/topicsComponent.js';
-import { Col, Row } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import Sidebar from '../sidebar.js';
+
 
 class Layout extends React.Component {
 
   render() {
-    return (
+    return ( 
       <div>
-        <div className="container height">
+        <Row>
+          <SumMetrics />
 
-          <Row>
-            <SumMetrics />
-          </Row>
-
-          <Row>
-            <Col xs={12} md={6}>
-              <Projects />
-            </Col>
-
-            <Col xs={12} md={6}>
-              <Stats />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={12} md={8}>
-              <Topics />
-            </Col>
-           </Row>
-
-        </div> {/*end of container*/}
+          <Sidebar />
+        </Row>
       </div>
     );
   }
