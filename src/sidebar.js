1e9677338bc3546cced011eef9b5472452a5fedf
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-07-31 15:37:42 +0300

    Add sidebar

---
+++ src/sidebar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Stats from './statistics/statsComponent.js';
+import Projects from './projects/projectsComponent.js';
+import Topics from './topics/topicsComponent.js';
+import Users from './users/usersComponent.js';
+import { Col, Row, Button } from 'react-bootstrap';
+
+
+const Sidebar = () => (
+  <Router>
+    <div>
+      <div style={{ width: '15%', background: '#f0f0f0' }}>
+        <ul style={{ listStyleType: 'none', padding: 0 }}>
+          <Button block><Link to="/projects" style={{ textDecoration: 'none', display: 'block' }}>Projects</Link></Button>
+          <Button block><Link to="/topics" style={{ textDecoration: 'none', display: 'block' }}>Topics</Link></Button>
+          <Button block><Link to="/statistics" style={{ textDecoration: 'none', display: 'block' }}>Operational Statistics</Link></Button>
+          <Button block><Link to="/users" style={{ textDecoration: 'none', display: 'block' }}>Users</Link></Button>
+        </ul>
+      </div>
+        <div className="container height">
+          <Row>
+            <Col xs={12} md={12}>
+              <Route exact path="/projects" component={Projects}/>
+            </Col>
+          </Row>
+          
+          <Row>
+            <Col xs={12} md={12}>
+              <Route exact path="/statistics" component={Stats}/>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={12} md={12}>
+              <Route exact path="/topics" component={Topics}/>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={12} md={12}>
+              <Route exact path="/users" component={Users}/>
+            </Col>
+          </Row>
+        </div>
+    </div>
+  </Router>
+)
+
+export default Sidebar
