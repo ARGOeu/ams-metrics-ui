937e32163558967545f30f8d748e7ced84d89bea
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-08-07 12:39:15 +0300

    Add multiple routes

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -2,19 +2,28 @@ import React from 'react';
 import SumMetrics from '../metrics/sumMetricsComponent.js';
 import { Row } from 'react-bootstrap';
 import Sidebar from '../sidebar.js';
+import ProjectsTab from '../projects/projects.js';
+import loginStore from '../login/loginStore.js';
+import Login from '../login/loginComponent.js';
+import Reflux from 'reflux';
+import { Switch, Route } from 'react-router-dom';
+import Admin from '../admin.js';
 
 
-class Layout extends React.Component {
+class Layout extends Reflux.Component {
+  constructor(props) {
+    super(props);
+    this.store = loginStore;
+  }
 
   render() {
-    return ( 
-      <div>
-        <Row>
-          <SumMetrics />
-
-          <Sidebar />
-        </Row>
-      </div>
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route exact path="/projects" component={ProjectsTab} />
+        <Route path="/projects/:project_name" component={ProjectsTab} />
+        <Route path="/admin" component={Admin} />
+      </Switch>
     );
   }
 }
