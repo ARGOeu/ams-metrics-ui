40caa51bb451ec5f2df63b1be8dc1aa4c9eff158
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-08-07 12:36:17 +0300

    Refactor file structure

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Stats from './statistics/statsComponent.js';
-import Projects from './projects/projectsComponent.js';
-import Topics from './topics/topicsComponent.js';
-import Users from './users/usersComponent.js';
+import Projects from './superAdminMetrics/projects/projectsComponent.js';
+import Topics from './superAdminMetrics/topics/topicsComponent.js';
+import Users from './superAdminMetrics/users/usersComponent.js';
 import { Col, Row, Button } from 'react-bootstrap';
 
 
@@ -18,31 +18,31 @@ const Sidebar = () => (
           <Button block bsStyle="warning"><NavLink to="/users" activeClassName="selected" className='button-link' style={{ textDecoration: 'none', display: 'block' }}>Users</NavLink></Button>
         </ul>
       </div>
-        <div className="container height">
-          <Row>
-            <Col xs={12} md={12}>
-              <Route exact path="/projects" component={Projects}/>
-            </Col>
-          </Row>
+      <div className="container height">
+        <Row>
+          <Col xs={12} md={12}>
+            <Route exact path="/projects" component={Projects}/>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col xs={12} md={12}>
-              <Route exact path="/statistics" component={Stats}/>
-            </Col>
-          </Row>
+        <Row>
+          <Col xs={12} md={12}>
+            <Route exact path="/statistics" component={Stats}/>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col xs={12} md={12}>
-              <Route exact path="/topics" component={Topics}/>
-            </Col>
-          </Row>
+        <Row>
+          <Col xs={12} md={12}>
+            <Route exact path="/topics" component={Topics}/>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col xs={12} md={12}>
-              <Route exact path="/users" component={Users}/>
-            </Col>
-          </Row>
-        </div>
+        <Row>
+          <Col xs={12} md={12}>
+            <Route exact path="/users" component={Users}/>
+          </Col>
+        </Row>
+      </div>
     </div>
   </Router>
 )
