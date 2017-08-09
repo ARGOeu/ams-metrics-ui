7ae2b0ee149a9a2a309a72999187fdd1a43b09b9
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-08-09 18:19:38 +0300

    Render projectItem component

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import SumMetrics from '../metrics/sumMetricsComponent.js';
-import { Row } from 'react-bootstrap';
-import Sidebar from '../sidebar.js';
 import ProjectsTab from '../projects/projects.js';
+import ProjectsItem from '../projects/projectsItem.js';
 import loginStore from '../login/loginStore.js';
 import Login from '../login/loginComponent.js';
 import Reflux from 'reflux';
@@ -21,7 +19,7 @@ class Layout extends Reflux.Component {
       <Switch>
         <Route path="/login" component={Login} />
         <Route exact path="/projects" component={ProjectsTab} />
-        <Route path="/projects/:project_name" component={ProjectsTab} />
+        <Route path="/projects/:project_name" component={ProjectsItem} />
         <Route path="/admin" component={Admin} />
       </Switch>
     );
