2de5216e3c61a8bf72611d4a374f680923300dc7
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-08-11 12:09:21 +0300

    Transform it to a simple react component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,8 @@ import './App.css';
 import Header from './static/header.js';
 import Footer from './static/footer.js';
 import Layout from './layout/layout.js';
-import Reflux from 'reflux';
-import loginStore from './login/loginStore.js';
 
-class App extends Reflux.Component {
-
-  constructor(props) {
-    super(props);
-    this.store = loginStore;
-  }
+class App extends React.Component {
 
   render() {
     return (
