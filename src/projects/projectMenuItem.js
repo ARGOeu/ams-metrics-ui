4f5ea9b6523c6c04c591f1479e0885cd84548b9b
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-09-14 16:53:23 +0300

    Fix layout
    Use wells to present list of projects

--- src/projects/projectMenuItem.js
+++ src/projects/projectMenuItem.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ListGroup, ListGroupItem, Col, Row } from 'react-bootstrap';
+import { Well, Col, Row } from 'react-bootstrap';
 
 
 class ProjectMenuItem extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <Row>
-          <Col xs={12} md={12}>
-            <ListGroup>
-              <ListGroupItem>
-                <Link to={`/projects/${this.props.project.project}`}>
-                  {this.props.project.project}
-                </Link>
-              </ListGroupItem>
-            </ListGroup>
-          </Col>
-        </Row>
+      <div className="item">
+        <Well>
+          <Link to={`/projects/${this.props.project.project}`}>
+            {this.props.project.project}
+          </Link>
+        </Well>
        </div>
     );
   }
