edbbc55f9eb98e2a24cd0fd3ff5b70fa192f34bc
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-09-15 14:29:14 +0300

    Add 1st version of sidebar
    Currently contains only operational statistics route

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,45 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Stats from './statistics/statsComponent.js';
-import Projects from './superAdminMetrics/projects/projectsComponent.js';
-import Topics from './superAdminMetrics/topics/topicsComponent.js';
-import Users from './superAdminMetrics/users/usersComponent.js';
+
 import { Col, Row, Button } from 'react-bootstrap';
 
 
 const Sidebar = () => (
   <Router>
     <div>
-      <div style={{ width: '15%', background: '#f0f0f0' }}>
+      <div style={{ width: '15%'}}>
         <ul style={{ listStyleType: 'none', padding: 0 }}>
-          <Button block bsStyle="warning"><NavLink to="/projects" activeClassName="selected" className='button-link' style={{ textDecoration: 'none', display: 'block' }}>Projects</NavLink></Button>
-          <Button block bsStyle="warning"><NavLink to="/topics" activeClassName="selected" className='button-link' style={{ textDecoration: 'none', display: 'block' }}>Topics</NavLink></Button>
-          <Button block bsStyle="warning"><NavLink to="/statistics" activeClassName="selected" className='button-link' style={{ textDecoration: 'none', display: 'block' }}>Operational Statistics</NavLink></Button>
-          <Button block bsStyle="warning"><NavLink to="/users" activeClassName="selected" className='button-link' style={{ textDecoration: 'none', display: 'block' }}>Users</NavLink></Button>
+          <Button block><NavLink to="/statistics" style={{ display: 'block' }}>Operational Statistics</NavLink></Button>
         </ul>
       </div>
       <div className="container height">
         <Row>
           <Col xs={12} md={12}>
-            <Route exact path="/projects" component={Projects}/>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12} md={12}>
             <Route exact path="/statistics" component={Stats}/>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12} md={12}>
-            <Route exact path="/topics" component={Topics}/>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12} md={12}>
-            <Route exact path="/users" component={Users}/>
           </Col>
         </Row>
       </div>
