0f16af252c7e97bfd5e3892d7a5d9915b94bd49b
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-09-26 17:01:22 +0300

    Cleanup css files

--- src/static/footer.js
+++ src/static/footer.js
@@ -1,40 +1,38 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 class Footer extends React.Component {
 
   render() {
     return (
-    <footer id="footer" className="footer">
+    <footer className="footer">
       <div className="footer_top">
-        <div className="row ">
-        </div>
-        <div className="row ">
-          <div className=" col-md-offset-1 col-md-3">
+        <Row>
+          <Col mdOffset={1} md={3}>
             <p><img src="./assets/argo-logo.png" alt="Argo"/></p>
             <p>ARGO is a lightweight service for Service Level Monitoring designed for medium and large sized e-Infrastructures. 
             Learn more: <a href='http://argoeu.github.io/' style={{color: 'white'}}>http://argoeu.github.io/</a></p>
-          </div>
-          <div className="col-md-offset-1 col-md-3">
+          </Col>
+          <Col mdOffset={1} md={3}>
             <h4>Contact Us</h4>
             <address>
             <strong>Email</strong><br/>
               <a href="mailto:argoeu-project@googlegroups.com" style={{color: 'white'}}>argoeu-project[AT]googlegroups.com</a>
             </address>
-          </div>
-          <div className="col-md-3">
+          </Col>
+          <Col md={3}>
             <h4>Open development </h4>
             <p> We follow an open development process. All the repositories of ARGO are hosted on Github under the ARGOeu organization.</p>
-          </div>
-          <div className="col-md-1"></div>
-        </div>
+          </Col>
+        </Row>
       </div>
-      
+
       <div className="footer_bottom_holder">
         <div className="footer_bottom">
-          <div className="textwidget">
+          <div>
             <span>Copyright © 2014 - 2016 GRNET S.A. </span>
           </div>
-          <div className="textwidget">
+          <div>
             <span>
               <a href="http://validator.w3.org/check?uri=referer" target="blank">
                 <img src="./assets/validated.png" alt="HTML5 - CSS3 - W3C Validated" title="HTML5 - CSS3 - W3C Validated" height="30"/>
