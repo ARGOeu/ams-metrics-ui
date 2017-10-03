0027d51664da6baedd80c4cf7498ec7665c7de23
Katerina Sarlou <ksarlou@callc.grnet.gr>
2017-10-03 10:57:12 +0300

    Fix labels in tab items

--- src/projects/projectTabItem.js
+++ src/projects/projectTabItem.js
@@ -6,12 +6,24 @@ import RoleInfo from './roleInfo.js';
 class ProjectTabItem extends React.Component {
 
   render() {
+
+    let roleLabel = "";
+    if (this.props.role === "service_admin") {
+      roleLabel = "Service Admin";
+    } else if (this.props.role === "project_admin") {
+      roleLabel = "Project Admin";
+    } else if (this.props.role === "consumer") {
+      roleLabel = "Consumer";
+    } else if (this.props.role === "publisher") {
+      roleLabel = "Publisher";
+    }
+
     return (
       <div>
         <Row>
           <Col xs={12} md={12}>
             <Tabs id="role-tab">
-              <Tab eventKey={1} title={this.props.role}><RoleInfo project={this.props.project} role={this.props.role}/></Tab>
+              <Tab eventKey={1} title={roleLabel}><RoleInfo project={this.props.project} role={this.props.role}/></Tab>
             </Tabs>
           </Col>
         </Row>
