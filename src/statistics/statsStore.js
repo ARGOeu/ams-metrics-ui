ffa0d1cc43fb8b107edc26716bfd3fd97f123ee9
Katerina Sarlou <ksarlou@callc.grnet.gr>
2018-06-13 14:41:03 +0300

    Fix bug about displaying operational statistics

--- src/statistics/statsStore.js
+++ src/statistics/statsStore.js
@@ -1,7 +1,7 @@
 import statsActions from './statsActions.js';
 import Reflux from 'reflux';
 import request from 'superagent';
-import { chain } from 'lodash';
+import { chain, groupBy, forOwn } from 'lodash';
 import { myConfig } from '../config.js';
 
 const metrics = `${myConfig.metricsUrl}`;
@@ -17,38 +17,33 @@ class StatsStore extends Reflux.Store {
   }
 
   onGetStats() {
-
-      request
-        .get(metrics)
-        .set('Content-Type', 'application/json')
-        .query({ key: myConfig.superAdmin })
-        .end((err, res) => {
-          if(err) throw err;
-          let stat_row = {};
-          let metrics = [];
-          let statMetrics = res.body.metrics;
-          const available_metrics = chain(statMetrics)
-            .map((metric) => {
-              return metric.metric.split(metric.resource_type + '.')[1];
-            })
-            .uniq()
-            .value();
-          //generate metrics table rows dynamically
-          statMetrics.forEach((metric) => {
-            stat_row.instanceName = metric.resource_name;
-          });
-
-          available_metrics.forEach((metric_type) => {
-            const found = statMetrics.find((metric) => { return (metric.metric.split(metric.resource_type + '.')[1] === metric_type)});
-            if(found) {
-              stat_row[metric_type] = (found.timeseries.length > 0) ? found.timeseries[0].value : 0;
-            } else {
-              stat_row[metric_type] = 0;
-            }
-          });
-          metrics.push(stat_row);
-          this.setState({ metrics });
-        });
+    request
+    .get(metrics)
+    .set('Content-Type', 'application/json')
+    .query({ key: myConfig.superAdmin })
+    .end((err, res) => {
+      if(err) throw err;
+      let metrics = [];
+      let statMetrics = res.body.metrics;
+      chain(statMetrics)
+      .groupBy(
+	(item) => {
+	  return item.resource_name;
+	}
+      )
+      .forOwn(
+	(value, key) => {
+	  let  metric = { instanceName: key };
+	  value.forEach((item) => {
+	    metric[item.metric.split('.')[1]] = (item.timeseries.length > 0) ? item.timeseries[0].value : 0;
+	  });
+	  metrics.push(metric);
+	  return metric;
+	}
+      )
+      .value();
+      this.setState({ metrics });
+    });
   }
 }
 
