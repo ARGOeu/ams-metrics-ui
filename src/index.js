f3c0f55402996588bb32ddae5c8264cd69929b06
Katerina Sarlou <ksarlou@callc.grnet.gr>
2018-10-25 15:59:26 +0300

    Add css of react-bootstrap-table2 &react-bootstrap-table2-paginator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
