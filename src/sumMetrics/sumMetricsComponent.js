0f76d16861d68e16ae70aaa94d72154bf883540b
Katerina Sarlou <ksarlou@callc.grnet.gr>
2018-11-09 15:18:54 +0200

    Implement dashboard panels

--- src/sumMetrics/sumMetricsComponent.js
+++ src/sumMetrics/sumMetricsComponent.js
@@ -2,8 +2,12 @@ import React from 'react';
 import sumMetricsActions from './sumMetricsActions.js';
 import sumMetricsStore from './sumMetricsStore.js';
 import Reflux from 'reflux';
-import { Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaUsers } from 'react-icons/fa';
+import { FaBriefcase } from 'react-icons/fa';
+import { IconContext } from "react-icons";
+
 
 class SumMetrics extends Reflux.Component {
   constructor(props) {
@@ -20,8 +24,36 @@ class SumMetrics extends Reflux.Component {
   render() {
     return (
       <div>
-        <Link className="panel" to={`/users`}>Users { this.state.users.length }</Link>
-        <Link className="panel" to={`/projects`}>Projects { this.state.projects.length }</Link>
+        <Container>
+          <Row>
+            <Col md={3}>
+              <div className="info-box">
+                <Link to={`/users`}>
+                  <IconContext.Provider value={{ className: "info-box-icon" }}>
+                    <span><FaUsers /></span>
+                  </IconContext.Provider>
+                  <div className="info-box-content">
+                    <span className="info-box-title">Users</span>
+                    <span className="info-box-text"> { this.state.users.length } </span>
+                  </div>
+                </Link>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className="info-box">
+                <Link to={`/projects`}>
+                  <IconContext.Provider value={{ className: "info-box-icon" }}>
+                    <span><FaBriefcase /></span>
+                  </IconContext.Provider>
+                  <div className="info-box-content">
+                    <span className="info-box-title">Projects</span>
+                    <span className="info-box-text"> { this.state.projects.length } </span>
+                  </div>
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
